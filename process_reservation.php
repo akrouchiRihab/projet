47f70003772b766da8a6c42e70d2d3d527303e42
Riihxab <95052897+Riihxab@users.noreply.github.com>
2024-01-28 16:06:10 +0100

    reservation

--- process_reservation.php
+++ process_reservation.php
@@ -1,6 +1,6 @@
 <?php
 // process_reservation.php
-
+session_start();
 require('includes/db_connect.php');
 
 // Check if rideID is provided in the form data
@@ -8,7 +8,12 @@ if (isset($_POST['ride_id'])) {
     $rideID = $_POST['ride_id'];
 
     // Assuming you have a user identifier, replace 'USER_ID' with the actual user identifier
-    $userID = 'USER_ID'; // Replace this with the actual user identifier or use your authentication system
+    if (isset($_SESSION["UserID"])) {
+        $userID = $_SESSION["UserID"];
+    } else {
+        echo 'echec';
+        exit; // Ajout pour arrêter l'exécution du script en cas d'échec
+    }
 
     // Check if the user has already made a reservation for this ride
     $sqlCheckReservation = "SELECT * FROM reservations WHERE RideID = '$rideID' AND UserID = '$userID'";
@@ -17,8 +22,8 @@ if (isset($_POST['ride_id'])) {
 
     if ($resultCheckReservation->num_rows > 0) {
         // User has already made a reservation for this ride
-        echo "Vous avez déjà réservé une place pour ce trajet.";
-        echo '<a href="reservation.php">voir mes reservation</a>';
+        echo '<script>alert("Vous avez déjà réservé une place pour ce trajet.");</script>';
+        
     } else {
         // User has not made a reservation for this ride, proceed with the reservation
         // Update the database to decrement the number of available seats
@@ -29,19 +34,20 @@ if (isset($_POST['ride_id'])) {
             $sqlInsertReservation = "INSERT INTO reservations (RideID, UserID) VALUES ('$rideID', '$userID')";
 
             if ($conn->query($sqlInsertReservation) === TRUE) {
-                echo "Réservé avec succès!";
-                echo '<a href="reservation.php">voir mes reservation</a>';
+                // Succès de la réservation
+                echo '<script>alert("Réservé avec succès! ");</script>';
             } else {
-                echo "Erreur lors de l'insertion de la réservation : " . $conn->error;
+                // Erreur lors de l'insertion de la réservation
+                echo '<script>alert("Erreur lors de l\'insertion de la réservation : ' . $conn->error . '");</script>';
             }
         } else {
-            echo "Erreur lors de la réservation : " . $conn->error;
+            // Erreur lors de la mise à jour des places disponibles
+            echo '<script>alert("Erreur lors de la mise à jour des places disponibles : ' . $conn->error . '");</script>';
         }
     }
-
-    // Close the database connection
-    $conn->close();
 } else {
     echo "Erreur : Aucun identifiant de trajet fourni.";
 }
+
+$conn->close();
 ?>
