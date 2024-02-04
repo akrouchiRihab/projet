8af5997c80eaea36a39a670e693b66f3435e5e69
ikram_ikram <154702668+12ikram@users.noreply.github.com>
2024-02-04 09:24:03 +0100

    commit

--- dashboard.php
+++ dashboard.php
@@ -15,7 +15,7 @@
          html, body {
                scroll-behavior: auto !important;
          }
-    </style>
+    </style>j
    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
